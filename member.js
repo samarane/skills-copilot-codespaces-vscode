69c97e7238d5145e3805cfe4908a8ed4d11277b9
samarane <101287235+samarane@users.noreply.github.com>
2024-03-18 20:24:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  return {
+    name: 'skillsMember',
+    restrict: 'E',
+    templateUrl: 'app/member/skills-member.html',
+    controller: 'SkillsMemberCtrl',
+    controllerAs: 'skillsMemberCtrl'
+  };
+}
